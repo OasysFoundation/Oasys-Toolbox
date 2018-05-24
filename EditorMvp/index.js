d54e1e813ae3babd3a541e1bcf7d7417762d1352
Frederik Riedel <frederik.riedel@frogg.io>
2018-05-24 13:22:03 +0200

    added new nicer buttons

--- EditorMvp/index.js
+++ EditorMvp/index.js
@@ -8,13 +8,16 @@ import './style/monokai-sublime.min.css'
 import './style/quill.snow.css'
 import './style/style.css'
 import {initQuill} from './editor.js'
+import {initQuiz} from './quizzer.js'
 
 
 
 
 initQuill();
+initQuiz();
 
 let slides = [];
+window.slides = slides
 let currentSlide = 0;
 let numberOfSlides = 1;
 
@@ -24,22 +27,41 @@ const switchToSlide = function(destination) {
     currentSlide = destination;
     window.quill.setContents(slides[destination]);
     const currentButton = document.getElementsByName("slide-button")[destination];
-    previousButton.style.background = currentButton.style.background;
-    currentButton.style.background = "#AAAAAA";
+    if (previousButton != null) {
+    	previousButton.setAttribute("class", "item");	
+    }
+    currentButton.setAttribute("class", "item active");
 }
 window.switchToSlide = switchToSlide;
+
+const saveContent = function() {
+	var xhr = new XMLHttpRequest();
+	var url = "http://api.joinoasys.org/saveEditor";
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.onreadystatechange = function () {
+	    if (xhr.readyState === 4 && xhr.status === 200) {
+	        var json = JSON.parse(xhr.responseText);
+	        console.log(json);
+	    }
+	};
+	var data = JSON.stringify(slides);
+	xhr.send(data);
+}
+window.saveContent = saveContent;
 
 
 document.getElementById('new-slide').addEventListener('click', function(e) {
 	numberOfSlides++;
-	let newSlideButton = document.createElement('button');
+	let newSlideButton = document.createElement('a');
+	newSlideButton.setAttribute("class", "item");
 	const currentSlideIndex = numberOfSlides-1;
+	slides[currentSlideIndex] = {"ops":[{"insert":"This is the beginning of the exiting journey of slide no " + numberOfSlides + "\n"}]}
 	newSlideButton.setAttribute("onclick", "switchToSlide("+ currentSlideIndex +")");
-	newSlideButton.setAttribute("style", "height:50px;width:100%;");
 	newSlideButton.setAttribute("name", "slide-button");
+	newSlideButton.setAttribute("style", "width:100%;");
 	newSlideButton.innerHTML = 'Slide ' + numberOfSlides;
-	document.getElementById('new-slide').parentNode.insertBefore(newSlideButton, document.getElementById('new-slide'));
+	document.getElementById('insert-new-slide-here').parentNode.insertBefore(newSlideButton, document.getElementById('insert-new-slide-here'));
+	switchToSlide(currentSlideIndex);
 });
 
-
-export {switchToSlide};
---
+++ EditorMvp/js/slickQuiz-config.js
@@ -0,0 +1,5 @@
+// Setup your quiz text and questions here
+
+// NOTE: pay attention to commas, IE struggles with those bad boys
+
+
